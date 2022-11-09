03d02ee5688ea974fba25a4c9bce5fa1a0dfafe3
mshahzad257 <114944060+mshahzad257@users.noreply.github.com>
2022-11-09 20:35:03 +0500

    update cors

--- server.js
+++ server.js
@@ -17,32 +17,36 @@ connectDB(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME);
 const app = express();
 
 // Middlewares
-// app.use(cors({
-//     origin: [
-//         'http://localhost:3000',
-//         'http://localhost:3001',
-//         'https://furnituremecca.vercel.app',
-//         'https://macca-dashboard.vercel.app',
-//         // 'https://ashley-api.herokuapp.com',
-//     ],
-//     optionsSuccessStatus: 200,
-//     credentials: true
-// }))
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://furnituremecca.vercel.app',
+        'https://mecca-dashboard.vercel.app/',
+        // 'https://ashley-api.herokuapp.com',
+    ],
+    optionsSuccessStatus: 200,
+    credentials: true
+}))
 
-app.use(cors());
-app.use(express.json({ limit: '200kb' }));
+// app.use(express.json({ limit: '200kb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+
 app.use('/uploads', express.static('src/assets/uploads'));
 
 // Port assign
 const port = process.env.PORT || 3000;
 
 // Get Environment
-const env = app.get('env');
+// const env = app.get('env');
 
-// morgan lib only for development environment logs only
-if (env === 'development') {
-    app.use(morgan('dev'));
-}
+// // morgan lib only for development environment logs only
+// if (env === 'development') {
+//     app.use(morgan('dev'));
+// }
 
 app.use("/",
     asyncHandler(async (req, res, next) => {
